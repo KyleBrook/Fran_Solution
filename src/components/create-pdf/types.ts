be99730d0e74dcbc6f61c83d0d74e32e5895418e
KyleBrook <kacavinhal@gmail.com>
2025-10-31 15:49:28 -0300

    [dyad] wrote 1 file(s)

---
+++ src/components/create-pdf/types.ts
@@ -0,0 +1,12 @@
+export type ImageItem = {
+  id: string;
+  src: string;
+  caption?: string;
+  widthPercent: number;
+  afterParagraph: number;
+};
+
+export type ParagraphOption = {
+  value: string;
+  label: string;
+};
